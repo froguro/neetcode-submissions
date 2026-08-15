5f5fdd6812a2c638a9ff6d26bc308670645de508
Owen Lam <114454864+froguro@users.noreply.github.com>
2026-08-15 12:50:35 -0700

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rowSet = defaultdict(set)
+        colSet = defaultdict(set)
+        squareSet = defaultdict(set)
+
+        for row in range(9):
+            for col in range(9):
+                val = board[row][col]
+                square = (row // 3) * 3 + (col // 3)
+                if val == '.':
+                    continue
+                elif val not in rowSet[row] and val not in colSet[col] and val not in squareSet[square]:
+                    squareSet[square].add(val)
+                    rowSet[row].add(val)
+                    colSet[col].add(val)
+                else:
+                    return False
+        return True
